e59b2061d79d0cf070a9953bfac75f09ca4d957e
Bhavesh Daipuria <bdaipuria@gmail.com>
2025-04-04 12:05:44 +0530

    feat: changes in the constituency and party model

--- models/constituency.js
+++ models/constituency.js
@@ -1,17 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const constituencySchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
-    state: { type: String, required: true },
-    totalVotes: { type: Number, required: true },
-    candidates: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Candidate' }],
-    won: {
-        type: String,
-        enum: ['won', 'loss', 'awaiting'],
-        default: 'awaiting'
-    },
+  name: { type: String, required: true, unique: true },
+  state: { type: String, required: true },
+  candidates: [{ type: mongoose.Schema.Types.ObjectId, ref: "Candidate" }],
 });
 
-const Constituency = mongoose.model('Constituency', constituencySchema);
+const Constituency = mongoose.model("Constituency", constituencySchema);
 
 module.exports = Constituency;
--- models/party.model.js
+++ models/party.model.js
@@ -1,15 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const partySchema = new mongoose.Schema({
   party: { type: String, required: true, unique: true },
-  total_seat: { type: Number, default: 0 },
-  total_votes: { type: Number, default: 0 },
-  electors: { type: Number, default: 0 },
-  color_code: { type: String, required: true },
+  color_code: { type: String, default: "#000000", required: true },
   party_logo: { type: String },
-  votes_percentage: { type: Number, default: 0 }
 });
 
-const Party = mongoose.model('Party', partySchema);
+const Party = mongoose.model("Party", partySchema);
 
 module.exports = Party;
